0d2d3227500c4e25dba8ed38864f626b3b30de7d
Dell <Dell@DESKTOP-GA2FP2D.lan>
2026-09-21 20:46:06 +0530

    tester

--- src/test/java/day16test/testclass16.java
+++ src/test/java/day16test/testclass16.java
@@ -1,5 +1,13 @@
 package day16test;
 
 public class testclass16 {
+	
+	public static void main(String args[]){
+		
+		System.out.println("good morning testing");
+		System.out.println("HI, ");
+		
+
+	}
 
 }
